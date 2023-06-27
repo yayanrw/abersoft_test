cdffe5fb2e0722beb6878802a9c29308fdcfbdb9
yayanrw <yayanraw@gmail.com>
2023-06-27 22:55:42 +0700

    run injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:abersoft_test/app/core/di/injection.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:logging/logging.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((event) {
     debugPrint(event.message);
   });
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Injection.init();
 
   runApp(
     GetMaterialApp(
