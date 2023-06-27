cd4663a1b550f8cd7ed08b3a7ff22b45adecaf6a
yayanrw <yayanraw@gmail.com>
2023-06-27 15:35:16 +0700

    using logger

--- lib/app/configs/networking/api_client.dart
+++ lib/app/configs/networking/api_client.dart
@@ -2,17 +2,18 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:logging/logging.dart';
 
 class ApiClient extends http.BaseClient {
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
-    debugPrint(
+    Logger.root.info(
         'request => ${request.method} (${request.url}) \n ${request.headers}');
     return request.send().then((value) {
       debugPrint('${value.statusCode} ${value.reasonPhrase}');
       return value;
     }).catchError((err) async {
-      debugPrint('error => ${err.toString()}');
+      Logger.root.warning('error => ${err.toString()}');
       return http.StreamedResponse(
           Stream.fromIterable([err.toString().codeUnits]), 500);
     });
@@ -21,10 +22,10 @@ class ApiClient extends http.BaseClient {
   @override
   Future<http.Response> get(Uri url, {Map<String, String>? headers}) {
     return super.get(url, headers: headers).then((value) {
-      debugPrint("response => ${value.body}");
+      Logger.root.info("response => ${value.body}");
       return value;
     }).catchError((err) {
-      debugPrint('error => ${err.toString()}');
+      Logger.root.warning('error => ${err.toString()}');
     });
   }
 
@@ -33,10 +34,10 @@ class ApiClient extends http.BaseClient {
       {Map<String, String>? headers, Object? body, Encoding? encoding}) {
     return post(url, headers: headers, body: body, encoding: encoding)
         .then((value) {
-      debugPrint('response => ${value.body}');
+      Logger.root.info("response => ${value.body}");
       return value;
     }).catchError((err) {
-      debugPrint('error => ${err.toString()}');
+      Logger.root.warning('error => ${err.toString()}');
     });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:logging/logging.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((event) {
+    debugPrint(event.message);
+  });
+
   runApp(
     GetMaterialApp(
       title: "Application",
