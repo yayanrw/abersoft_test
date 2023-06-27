c4183d2c51084fe8153f29c448ca4df286716b8b
yayanrw <yayanraw@gmail.com>
2023-06-27 16:20:21 +0700

    add user_credentials.dart

---
+++ lib/app/domain/entities/user_credentials.dart
@@ -0,0 +1,21 @@
+import 'package:abersoft_test/app/data/datasources/index.dart';
+import 'package:equatable/equatable.dart';
+
+class UserCredentials extends Equatable {
+  final bool isLoggedIn;
+  final String token;
+
+  const UserCredentials({
+    required this.isLoggedIn,
+    required this.token,
+  });
+
+  @override
+  List<Object?> get props => [isLoggedIn, token];
+}
+
+extension UserCredentialsPreferenceExt on UserCredentialsPreference {
+  UserCredentials toEntity() {
+    return UserCredentials(isLoggedIn: isLoggedIn, token: token ?? "N/A");
+  }
+}
