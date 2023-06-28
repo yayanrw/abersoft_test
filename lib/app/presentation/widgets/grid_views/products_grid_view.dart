510f874848366fd1dcf041f130eec8eaba120012
yayanrw <yayanraw@gmail.com>
2023-06-28 18:44:26 +0700

    add products_grid_view.dart

---
+++ lib/app/presentation/widgets/grid_views/products_grid_view.dart
@@ -0,0 +1,37 @@
+import 'package:abersoft_test/app/domain/entities/product.dart';
+import 'package:abersoft_test/app/presentation/widgets/images/product_image_widget.dart';
+import 'package:flutter/material.dart';
+
+class ProductsGridView extends StatelessWidget {
+  const ProductsGridView({super.key, required this.products});
+
+  final List<Product> products;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 19),
+      child: GridView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisExtent: 96,
+          mainAxisSpacing: 27,
+          crossAxisSpacing: (MediaQuery.of(context).size.width / 5) + 4,
+          childAspectRatio: 1,
+        ),
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final Product product = products[index];
+
+          return ProductImageWidget(
+            key: Key("product_grid_${product.id.toString()}"),
+            imgUrl: product.imageUrl,
+            isLastItem: true,
+          );
+        },
+      ),
+    );
+  }
+}
