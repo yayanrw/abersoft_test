a20bc49c491dbf0548587bbec7fb358e4705943e
yayanrw <yayanraw@gmail.com>
2023-06-28 18:38:12 +0700

    add produ

---
+++ lib/app/presentation/widgets/list_views/products_list_view.dart
@@ -0,0 +1,32 @@
+import 'package:abersoft_test/app/domain/entities/product.dart';
+import 'package:abersoft_test/app/presentation/widgets/images/product_image_widget.dart';
+import 'package:flutter/material.dart';
+
+class ProductsListView extends StatelessWidget {
+  const ProductsListView({super.key, required this.products});
+
+  final List<Product> products;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 96,
+      child: ListView.builder(
+        physics: const AlwaysScrollableScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        itemCount: products.length,
+        itemBuilder: (BuildContext context, int index) {
+          final Product product = products[index];
+          final isLastItem = index == products.length - 1;
+
+          return ProductImageWidget(
+            key: Key("product_horizontal_${product.id.toString()}"),
+            imgUrl: product.imageUrl,
+            isLastItem: isLastItem,
+          );
+        },
+      ),
+    );
+  }
+}
