5d0384c510a0d01b7800a4f377e842b9c3455408
yayanrw <yayanraw@gmail.com>
2023-06-28 09:14:03 +0700

    update login

--- lib/app/presentation/pages/log_in/log_in_controller.dart
+++ lib/app/presentation/pages/log_in/log_in_controller.dart
@@ -1,23 +1,41 @@
+import 'package:abersoft_test/app/core/utils/network_helper.dart';
+import 'package:abersoft_test/app/core/utils/snackbar_helper.dart';
+import 'package:abersoft_test/app/data/datasources/index.dart';
+import 'package:abersoft_test/app/domain/usecases/log_in.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class LogInController extends GetxController {
-  //TODO: Implement LogInController
+  final LogIn _logIn = Get.find<LogIn>();
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  RxBool isLoading = false.obs;
 
   @override
-  void onClose() {
+  void onDispose() {
+    usernameController.dispose();
+    passwordController.dispose();
     super.onClose();
   }
 
-  void increment() => count.value++;
+  void handleLogin() async {
+    isLoading.value = true;
+    await NetworkHelper.callDataService<String>(
+      () => _logIn.exec(
+        LoginRequest(
+          username: usernameController.text.trim(),
+          password: passwordController.text.trim(),
+        ),
+      ),
+      onSuccess: ((response) async {
+        SnackBarHelper.success(
+            message: "Login successfully, redirecting to home page.");
+      }),
+      onDone: () async {
+        isLoading.value = false;
+      },
+    );
+  }
 }
--- lib/app/presentation/pages/log_in/log_in_view.dart
+++ lib/app/presentation/pages/log_in/log_in_view.dart
@@ -1,22 +1,56 @@
+import 'package:abersoft_test/app/presentation/widgets/button_widget.dart';
+import 'package:abersoft_test/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import 'log_in_controller.dart';
 
 class LogInView extends GetView<LogInController> {
   const LogInView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LogInView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'LogInView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 66),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 84),
+                  Assets.images.abersoft.image(height: 53),
+                  const SizedBox(height: 119),
+                  TextFormField(
+                    controller: controller.usernameController,
+                    decoration: const InputDecoration(
+                      hintText: "Username",
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: controller.passwordController,
+                    decoration: const InputDecoration(
+                      hintText: "Password",
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  Obx(() {
+                    return ButtonWidget(
+                      text: "LOGIN",
+                      isLoading: controller.isLoading.value,
+                      onPressed: () {
+                        controller.handleLogin();
+                      },
+                    );
+                  })
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
