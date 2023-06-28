38ca881af34af44fa542f782395adc32df66ca0e
yayanrw <yayanraw@gmail.com>
2023-06-28 09:06:57 +0700

    add success

--- lib/app/core/utils/snackbar_helper.dart
+++ lib/app/core/utils/snackbar_helper.dart
@@ -11,9 +11,26 @@ class SnackBarHelper {
       message,
       margin: const EdgeInsets.all(16),
       icon: const Icon(
-        Icons.error,
+        Icons.error_rounded,
         color: Colors.red,
-        key: Key("server_error"),
+        key: Key("snack_bar_error"),
+      ),
+      snackPosition: SnackPosition.BOTTOM,
+    );
+  }
+
+  static success({
+    String title = "Success",
+    String message = "Successfully",
+  }) {
+    Get.snackbar(
+      title,
+      message,
+      margin: const EdgeInsets.all(16),
+      icon: const Icon(
+        Icons.check_circle,
+        color: Colors.green,
+        key: Key("snack_bar_success"),
       ),
       snackPosition: SnackPosition.BOTTOM,
     );
