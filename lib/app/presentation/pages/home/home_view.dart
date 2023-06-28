bf6194fe74d7b466fd09e06333f872dcbbc5574c
yayanrw <yayanraw@gmail.com>
2023-06-28 19:17:51 +0700

    add refreshindicator

--- lib/app/presentation/pages/home/home_view.dart
+++ lib/app/presentation/pages/home/home_view.dart
@@ -15,54 +15,59 @@ class HomeView extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Abersoft Flutter Test"),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () {
-              controller.logOut();
-            },
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(child: GetBuilder<HomeController>(
-          builder: (controller) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 19),
-                const TitleWidget(text: "Our Portfolio"),
-                const SizedBox(height: 21),
-                const SubTitleWidget(text: "Best Product"),
-                const SizedBox(height: 35),
-                controller.isLoading
-                    ? const ProductHorizontalShimmerWidget()
-                    : ProductsListView(products: controller.bestProducts),
-                const SizedBox(height: 28),
-                const SubTitleWidget(text: "All Product"),
-                const SizedBox(height: 21),
-                controller.isLoading
-                    ? const ProductsGridShimmerWidget()
-                    : ProductsGridView(products: controller.allProducts),
-                const SizedBox(height: 24),
-              ],
-            );
-          },
-        )),
-      ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 24),
-        child: FloatingActionButton(
-          onPressed: () {
-            Get.toNamed(Routes.CREATE_PRODUCT);
-          },
-          child: const Icon(Icons.add),
+    return GetBuilder<HomeController>(builder: (controller) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Abersoft Flutter Test"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout),
+              onPressed: () {
+                controller.logOut();
+              },
+            ),
+          ],
         ),
-      ),
-    );
+        body: SafeArea(
+          child: RefreshIndicator(
+            onRefresh: () async {
+              await controller.getProducts();
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 19),
+                  const TitleWidget(text: "Our Portfolio"),
+                  const SizedBox(height: 21),
+                  const SubTitleWidget(text: "Best Product"),
+                  const SizedBox(height: 35),
+                  controller.isLoading
+                      ? const ProductHorizontalShimmerWidget()
+                      : ProductsListView(products: controller.bestProducts),
+                  const SizedBox(height: 28),
+                  const SubTitleWidget(text: "All Product"),
+                  const SizedBox(height: 21),
+                  controller.isLoading
+                      ? const ProductsGridShimmerWidget()
+                      : ProductsGridView(products: controller.allProducts),
+                  const SizedBox(height: 24),
+                ],
+              ),
+            ),
+          ),
+        ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: 24),
+          child: FloatingActionButton(
+            onPressed: () {
+              Get.toNamed(Routes.CREATE_PRODUCT);
+            },
+            child: const Icon(Icons.add),
+          ),
+        ),
+      );
+    });
   }
 }
