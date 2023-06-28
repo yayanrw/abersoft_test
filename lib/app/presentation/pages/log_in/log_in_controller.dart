14b8d3ee78f2ae80f0ff7c49ed12f0a5a622ffcc
yayanrw <yayanraw@gmail.com>
2023-06-28 09:47:01 +0700

    redirect when has a logged in

--- lib/app/presentation/pages/log_in/log_in_controller.dart
+++ lib/app/presentation/pages/log_in/log_in_controller.dart
@@ -1,6 +1,7 @@
 import 'package:abersoft_test/app/core/utils/network_helper.dart';
 import 'package:abersoft_test/app/core/utils/snackbar_helper.dart';
 import 'package:abersoft_test/app/data/datasources/index.dart';
+import 'package:abersoft_test/app/domain/repositories/user_credentials_repository.dart';
 import 'package:abersoft_test/app/domain/usecases/log_in.dart';
 import 'package:abersoft_test/app/routes/app_pages.dart';
 import 'package:flutter/cupertino.dart';
@@ -8,11 +9,23 @@ import 'package:get/get.dart';
 
 class LogInController extends GetxController {
   final LogIn _logIn = Get.find<LogIn>();
+  final UserCredentialsRepository _userCredentialsRepository =
+      Get.find<UserCredentialsRepository>();
 
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
   RxBool isLoading = false.obs;
+
+  @override
+  void onInit() async {
+    super.onInit();
+    final userCredentials =
+        await _userCredentialsRepository.getUserCredentials();
+    if (userCredentials.isLoggedIn) {
+      Get.offAllNamed(Routes.HOME);
+    }
+  }
 
   @override
   void dispose() {
