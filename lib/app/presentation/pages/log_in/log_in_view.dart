cc40a48a7e108e1db888b323425a540fae510aaa
yayanrw <yayanraw@gmail.com>
2023-06-28 09:36:14 +0700

    add validation

--- lib/app/presentation/pages/log_in/log_in_view.dart
+++ lib/app/presentation/pages/log_in/log_in_view.dart
@@ -1,6 +1,7 @@
 import 'package:abersoft_test/app/presentation/widgets/button_widget.dart';
 import 'package:abersoft_test/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:form_validator/form_validator.dart';
 import 'package:get/get.dart';
 
 import 'log_in_controller.dart';
@@ -10,6 +11,8 @@ class LogInView extends GetView<LogInController> {
 
   @override
   Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -17,37 +20,51 @@ class LogInView extends GetView<LogInController> {
             padding: const EdgeInsets.symmetric(horizontal: 66),
             child: SizedBox(
               width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 84),
-                  Assets.images.abersoft.image(height: 53),
-                  const SizedBox(height: 119),
-                  TextFormField(
-                    controller: controller.usernameController,
-                    decoration: const InputDecoration(
-                      hintText: "Username",
+              child: Form(
+                key: formKey,
+                autovalidateMode: AutovalidateMode.always,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 84),
+                    Assets.images.abersoft.image(height: 53),
+                    const SizedBox(height: 119),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      controller: controller.usernameController,
+                      decoration: const InputDecoration(
+                        hintText: "Username",
+                      ),
+                      validator: ValidationBuilder().required().build(),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  TextFormField(
-                    controller: controller.passwordController,
-                    decoration: const InputDecoration(
-                      hintText: "Password",
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      controller: controller.passwordController,
+                      decoration: const InputDecoration(
+                        hintText: "Password",
+                      ),
+                      validator: ValidationBuilder().required().build(),
                     ),
-                  ),
-                  const SizedBox(height: 25),
-                  Obx(() {
-                    return ButtonWidget(
-                      text: "LOGIN",
-                      isLoading: controller.isLoading.value,
-                      onPressed: () {
-                        controller.handleLogin();
+                    const SizedBox(height: 25),
+                    Obx(
+                      () {
+                        return ButtonWidget(
+                          text: "LOGIN",
+                          isLoading: controller.isLoading.value,
+                          onPressed: () {
+                            final form = formKey.currentState;
+                            if (form != null && form.validate()) {
+                              controller.handleLogin();
+                            }
+                          },
+                        );
                       },
-                    );
-                  })
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
